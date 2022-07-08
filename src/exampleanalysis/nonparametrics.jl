eaa3e2c8453a56ce53a57375d8345f24f5a35d60
JakeGrainger <j.p.grainger2@outlook.com>
2022-07-08 21:40:52 +0100

    Add nw specification.

--- src/exampleanalysis/nonparametrics.jl
+++ src/exampleanalysis/nonparametrics.jl
@@ -1,6 +1,6 @@
-function mt_wavedirection(signal::AbstractMatrix{T}, n, n_overlap; fs=1,onesided=true) where {T}
-    config = DSP.MTSpectrogramConfig{T}(size(signal,2), n, n_overlap; fs=fs,onesided=onesided)
-    configcross = DSP.MTCrossSpectraConfig{T}(size(signal,1), n; fs = fs,onesided=onesided)
+function mt_wavedirection(signal::AbstractMatrix{T}, n, n_overlap; fs=1,onesided=true, nw::Real=4, ntapers::Int=ceil(Int, 2nw)-1) where {T}
+    config = DSP.MTSpectrogramConfig{T}(size(signal,2), n, n_overlap; fs=fs,onesided=onesided,nw=nw,ntapers=ntaper)
+    configcross = DSP.MTCrossSpectraConfig{T}(size(signal,1), n; fs = fs,onesided=onesided,nw=nw,ntapers=ntaper)
     X = DSP.allocate_output(config)
     tempoutput = DSP.allocate_output(configcross)
     epochs = [DSP.arraysplit(r,n,n_overlap) for r in eachrow(signal)]
@@ -17,7 +17,7 @@ end
 
 function compute_spectrogram(waveseries, wavetime)
     continuouswave = reduce(vcat, w[:,1] for w in waveseries)
-    spec = mt_spectrogram(continuouswave, 2304, 2304÷2, fs = 1) # fs has to be int for some reason
+    spec = mt_spectrogram(continuouswave, 2304, 2304÷2, fs = 1, nw=4, ntapers=7) # fs has to be int for some reason
     spectrogram = spec.power./(2π * 1.28) ./2 # equivalent to * Δ/2π and div 2 because onesided
     spectime = wavetime[1]:(wavetime[2]-wavetime[1])/2:wavetime[end]
     Ω = collect((2π*1.28) .* spec.freq)
@@ -26,7 +26,7 @@ end
 
 function compute_errorgram(waveseries, wavetime)
     continuouswave = [reduce(vcat, w[:,i] for w in waveseries) for i in 1:3]
-    spec = [mt_spectrogram(c, 2304, 0) for c in continuouswave]
+    spec = [mt_spectrogram(c, 2304, 0, nw=4, ntapers=7) for c in continuouswave]
     spectrogram = [s.power./(2π * 1.28) ./2 for s in spec] # equivalent to * Δ/2π and div 2 because onesided (DSP only allows spectrogram with integer fs so rescale is necessary)
     Ω = collect((2π*1.28) .* spec[1].freq)
     errorgram = @. log(spectrogram[2]+spectrogram[3])+2*log(tanh(OceanWaveSpectralFitting.approx_dispersion(Ω,40)))-log(spectrogram[1])
